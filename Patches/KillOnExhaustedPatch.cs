7d46f99e1ca7852e833b2654039ab32c8bede8a0
skreenz <lafaye.alexis@gmail.com>
2024-08-22 23:49:23 +0200

    Initial commit

---
+++ Patches/KillOnExhaustedPatch.cs
@@ -0,0 +1,20 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace TobogangMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    public class KillOnExhaustedPatch
+    {
+        [HarmonyPatch(nameof(PlayerControllerB.Update))]
+        [HarmonyPostfix]
+        private static void UpdatePostfix(PlayerControllerB __instance)
+        {
+            if (__instance.isExhausted)
+            {
+                __instance.KillPlayer(Vector3.zero);
+            }
+        }
+    }
+}
