189ef546db6f10c354eab1ec44a6ca4ba2e6044b
skreenz <lafaye.alexis@gmail.com>
2024-08-25 22:31:12 +0200

    Add Cramptes WIP

--- TobogangMod.cs
+++ TobogangMod.cs
@@ -10,11 +10,15 @@ using LethalLib.Modules;
 using TobogangMod.Scripts;
 using Unity.Netcode;
 using System.Reflection;
+using static LethalLib.Modules.ContentLoader;
+using System.Collections.Generic;
+using LethalLib.Extras;
 
 namespace TobogangMod;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency("BMX.LobbyCompatibility", BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency(LethalLib.Plugin.ModGUID)]
 [LobbyCompatibility(CompatibilityLevel.ClientOnly, VersionStrictness.None)]
 public class TobogangMod : BaseUnityPlugin
 {
@@ -23,18 +27,26 @@ public class TobogangMod : BaseUnityPlugin
     internal static Harmony? Harmony { get; set; }
     public static AssetBundle MainAssetBundle { get; private set; } = null!;
 
+    public static ContentLoader ContentLoader;
+    public static Dictionary<string, GameObject> Prefabs = new Dictionary<string, GameObject>();
+
     private void Awake()
     {
+        Instance = this;
+        Logger = base.Logger;
+
         var dllFolderPath = System.IO.Path.GetDirectoryName(Info.Location);
-        var assetBundleFilePath = System.IO.Path.Combine(dllFolderPath, "TobogangAsset");
+        var assetBundleFilePath = System.IO.Path.Combine(dllFolderPath, "tobogangasset");
         MainAssetBundle = AssetBundle.LoadFromFile(assetBundleFilePath);
 
-        NetcodePatcher();
+        ContentLoader = new ContentLoader(Instance.Info, MainAssetBundle, (content, prefab) => {
+            Prefabs.Add(content.ID, prefab);
+        });
 
-        Logger = base.Logger;
-        Instance = this;
-
+        CreateItems();
         Patch();
+
+        NetcodePatcher();
 
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded !");
     }
@@ -48,6 +60,16 @@ public class TobogangMod : BaseUnityPlugin
         Harmony.PatchAll();
 
         Logger.LogDebug("Finished patching!");
+    }
+
+    void CreateItems()
+    {
+        ContentLoader.Register(new ScrapItem("Cramptes", "Assets/CustomAssets/CramptesItem.asset", 1000, Levels.LevelTypes.All, null, (Item item) => {
+            CramptesItem script = item.spawnPrefab.AddComponent<CramptesItem>();
+            script.itemProperties = item;
+        }));
+
+        Logger.LogInfo("Registered items");
     }
 
     internal static void Unpatch()
